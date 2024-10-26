b24fe19d7d9ac2caa9b31ef759843d8a492e7577
Abhinesh <work.abhinesh@gmail.com>
2024-10-26 16:38:11 +0530

    Add files via upload

---
+++ precache-manifest.f907f2c41c9415269ab88267a4cf3465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992be56e7a0a061d75e2868fb670ac7f",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "8829ac0ac92f8ec14244",
+    "url": "/quotes/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "ff04d4b4d9e5334b18f3",
+    "url": "/quotes/static/css/main.cf2e0138.chunk.css"
+  },
+  {
+    "revision": "8829ac0ac92f8ec14244",
+    "url": "/quotes/static/js/2.cdb49c9a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/quotes/static/js/2.cdb49c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff04d4b4d9e5334b18f3",
+    "url": "/quotes/static/js/main.e6cbabc7.chunk.js"
+  },
+  {
+    "revision": "ca062f6969848655985f",
+    "url": "/quotes/static/js/runtime-main.332365af.js"
+  }
+]);
